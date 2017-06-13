7ca92f0f510a91a094c73dc2ee8d4ec33e7671d9
blessedsoy <unmigong@gmail.com>
2017-06-12 22:39:25 -0400

    created a bar graph using d3

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Nav from './nav.js'
+import Navigation from './navigation.js';
+import BarChart from '../components/bar-chart.js';
+import CircleChart from '../components/circle-chart.js';
 
 export default class App extends Component {
   render() {
     return(
       <div>
-        <Nav />
+        <Navigation />
+        <BarChart />
+        <CircleChart />
       </div>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
-
+import BarChartReducer from './reducer_bar_chart'
+import CircleChartReducer from './reducer_circle_chart'
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  barChartData: BarChartReducer,
+  circleChartData: CircleChartReducer
 });
 
 export default rootReducer;
